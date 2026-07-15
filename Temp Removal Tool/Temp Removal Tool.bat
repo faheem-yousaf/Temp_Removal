c593d7b536d9ca6189efb9b39d1a603445f76675
Faheem Yousaf <yousaff2@NETSOLPK.COM>
2026-07-15 16:10:25 +0500

    First Commit

---
+++ Temp Removal Tool/Temp Removal Tool.bat
@@ -0,0 +1,7 @@
+@echo off
+	del "%tmp%\*.*" /s /q /f
+	FOR /d %%p IN ("%tmp%\*.*") DO rmdir "%%p" /s /q
+	del "%windir%\Temp\*.*" /s /q /f
+	FOR /d %%p IN ("%windir%\Temp\*.*") DO rmdir "%%p" /s /q
+	del "%windir%\Installer\*.msi" /s /q /f
+	del "%windir%\Installer\*.msp" /s /q /f
